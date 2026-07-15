d1e01656e73b8ebbedbac7f14abc1076d83e7261
ZulfaSayidahR <zulfasayidahr@gmail.com>
2026-07-15 08:16:32 +0700

    Update halaman

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,83 +5,50 @@
 
     <meta charset="UTF-8">
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>
         @yield('title', 'Kopi CeTe')
     </title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-    {{-- ===============================
-    GOOGLE FONT
-    ================================ --}}
+    {{-- Google Font --}}
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
         rel="stylesheet">
 
-
-    {{-- ===============================
-    BOOTSTRAP
-    ================================ --}}
+    {{-- Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-
-    {{-- ===============================
-    BOOTSTRAP ICON
-    ================================ --}}
+    {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
+    {{-- CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-    {{-- ===============================
-    CUSTOM CSS
-    ================================ --}}
-
-    {{-- Global CSS --}}
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-
-    {{-- Page CSS --}}
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
     @stack('styles')
 
-
 </head>
-
 
 <body>
 
-
-    {{-- ===============================
-    NAVBAR
-    ================================ --}}
-
+    {{-- Navbar --}}
     @include('layouts.navbar')
 
-
-
-    {{-- ===============================
-    CONTENT
-    ================================ --}}
-
+    {{-- Content --}}
     <main>
 
         @yield('content')
 
     </main>
 
+    {{-- Footer --}}
+    @include('layouts.footer')
 
-    @extends('layouts.footer')
-    {{-- ===============================
-    JAVASCRIPT
-    ================================ --}}
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
-    </script>
-
+    {{-- Bootstrap JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     @stack('scripts')
-
 
 </body>
 
