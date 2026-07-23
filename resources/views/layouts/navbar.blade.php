c786e711be8f20e1a4da200773c5ab20deb7aa55
ZulfaSayidahR <zulfasayidahr@gmail.com>
2026-07-23 10:21:16 +0700

    responsive toggle

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,100 +1,88 @@
-<nav class="navbar navbar-expand-lg navbar-bnn">
+<nav class="navbar navbar-expand-lg navbar-dark navbar-bnn">
 
+    <div class="container">
 
-    <div class="container px-5">
+        {{-- Logo --}}
+        <a href="{{ route('home') }}" class="navbar-brand logo-group">
 
+            <img src="{{ asset('images/bnn.webp') }}" class="logo-bnn" alt="BNN">
+            <img src="{{ asset('images/warondrugs.jpg') }}" class="logo-war" alt="War On Drugs">
+            <img src="{{ asset('images/kopicete.png') }}" class="logo-kopicete" alt="Kopicete">
 
-        <div class="d-flex justify-content-between align-items-center w-100">
-
-        <a href="{{ route('home') }}" class="d-flex align-items-center logo-group">
-            <img src="{{ asset('images/bnn.webp') }}" class="logo-bnn">
-            <img src="{{ asset('images/warondrugs.jpg') }}" class="logo-war">
-            <img src="{{ asset('images/kopicete.png') }}" class="logo-kopicete">
         </a>
 
-        <button class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#menu"
-            aria-controls="menu"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+        {{-- Toggle --}}
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu"
+            aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
 
+            <span class="navbar-toggler-icon"></span>
 
+        </button>
 
+        {{-- Menu --}}
         <div class="collapse navbar-collapse" id="menu">
 
-
-            <ul class="navbar-nav ms-auto">
-
+            <ul class="navbar-nav ms-auto align-items-lg-center">
 
                 <li class="nav-item">
-
-                    <a class="nav-link" href="/">
+                    <a class="nav-link" href="{{ route('home') }}">
                         Beranda
                     </a>
-
                 </li>
 
-
-
                 <li class="nav-item">
-
-                    <a class="nav-link" href="/pengaduan">
-
+                    <a class="nav-link" href="{{ route('pengaduan.create') }}">
                         Pengaduan
-
                     </a>
-
                 </li>
-
-
 
                 <li class="nav-item">
-
-                    <a class="nav-link" href="/permohonan">
-
+                    <a class="nav-link" href="{{ route('permohonan.create') }}">
                         Permohonan
-
                     </a>
-
                 </li>
 
-
-
-                {{-- <li class="nav-item">
-
-                    <a class="nav-link" href="/tracking">
-
-                        Lacak Aduan
-
-                    </a>
-
-                </li> --}}
-
-
-                <li class="nav-item ms-3">
-
-                    <a href="{{ route('login') }}" class="btn btn-yellow">
-
+                <li class="nav-item ms-lg-3 mt-3 mt-lg-0">
+                    <a href="{{ route('login') }}" class="btn btn-yellow w-100">
                         Masuk Admin
-
                     </a>
-
                 </li>
-
-
 
             </ul>
 
-
-
         </div>
-
 
     </div>
 
-
 </nav>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+
+        const menu = document.getElementById('menu');
+        const toggler = document.querySelector('.navbar-toggler');
+
+        if (menu && toggler) {
+
+            // Tutup menu saat salah satu link diklik
+            document.querySelectorAll('#menu .nav-link').forEach(link => {
+                link.addEventListener('click', () => {
+                    if (window.innerWidth < 992) {
+                        bootstrap.Collapse.getOrCreateInstance(menu).hide();
+                    }
+                });
+            });
+
+            // Sinkronkan atribut aria-expanded
+            menu.addEventListener('shown.bs.collapse', () => {
+                toggler.setAttribute('aria-expanded', 'true');
+            });
+
+            menu.addEventListener('hidden.bs.collapse', () => {
+                toggler.setAttribute('aria-expanded', 'false');
+            });
+
+        }
+
+    });
+</script>
